27a5a1a7aba5f54d2d04363c904978c68cdf7949
Robert Schoefbeck <robertschoefbeck@pb-d-128-141-161-242.cern.ch>
2016-02-08 14:33:21 +0100

    fix logger

--- example_variables.py
+++ example_variables.py
@@ -1,5 +1,22 @@
 from Variable import Variable, ScalarType, VectorType
 
+# Logger
+from logger import get_logger
+
+# argParser
+import argparse
+argParser = argparse.ArgumentParser(description = "Argument parser")
+argParser.add_argument('--logLevel', 
+      action='store',
+      nargs='?',
+      choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],
+      default='INFO',
+      help="Log level for logging"
+)
+
+args = argParser.parse_args()
+logger = get_logger(args.logLevel, logFile = None)
+
 s1 = ScalarType('x', 'F')
 s2 = Variable.fromString('phi/F')
 s3 = Variable.fromString('y/I')
