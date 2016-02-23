db90f7bb4f18815b86b71b457a22cd15d854f948
schoefbeck <robert.schoefbeck@cern.ch>
2016-02-23 09:36:09 +0100

    add Plot.fromHisto to example_treeReader

--- examples/example_treeReader.py
+++ examples/example_treeReader.py
@@ -7,6 +7,8 @@ from RootTools.core.Sample import Sample
 from RootTools.core.Variable import Variable, ScalarType, VectorType
 from RootTools.core.TreeReader import TreeReader
 from RootTools.core.logger import get_logger
+from RootTools.plot.Plot import Plot
+import RootTools.plot.plotting as plotting
 
 # argParser
 import argparse
@@ -31,14 +33,18 @@ variables =  [ Variable.fromString('Jet[pt/F,eta/F,phi/F]' ) ] \
 #s1.reader( scalars = scalars, vectors = vectors, selectionString = "met_pt>100")
 
 h=ROOT.TH1F('met','met',100,0,0)
-r = s0.treeReader( variables = variables, selectionString = "met_pt>100")
+r = s0.treeReader( variables = variables, selectionString = "met_pt>100" )
 r.start()
 while r.run():
     h.Fill( r.data.met_pt )
 
 # No selection string in reader
 h_inclusive=ROOT.TH1F('met_inclusive','met',100,0,0)
-r = s0.treeReader( variables = variables)
+r = s0.treeReader( variables = variables )
 r.start()
 while r.run():
     h_inclusive.Fill( r.data.met_pt )
+
+# Make plot
+plot1 = Plot.fromHisto(name = "met", histos = [[h_inclusive]], texX = "#slash{E}_{T} (GeV", texY = "Number of events" )
+plotting.draw(plot1, plot_directory = ".", ratio = None, logX = False, logY = False, sorting = False )
