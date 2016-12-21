de6ca1e64a4a05f152b2c42e4e1725f1f2d9c1c3
schoefbeck <robert.schoefbeck@cern.ch>
2016-12-21 15:53:43 +0100

    bugfix

--- plot/python/plotting.py
+++ plot/python/plotting.py
@@ -25,7 +25,7 @@ def copyIndexPHP( directory ):
     index_php = os.path.join( directory, 'index.php' )
     if not os.path.exists( directory ): os.makedirs( directory )  
     if not os.path.exists( index_php ):
-      shutil.copyfile( os.path.expandvars( '$CMSSW_BASE/src/StopsDilepton/plot/php/index.php' ), index_php )
+      shutil.copyfile( os.path.expandvars( '$CMSSW_BASE/src/RootTools/plot/php/index.php' ), index_php )
 
 
 def getLegendMaskedArea(legend_coordinates, pad):
