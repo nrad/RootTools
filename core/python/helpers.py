75a9270187cf91b90bdc326e404bbd5e7c6ae7ee
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-02-20 15:15:31 +0100

    renew_proxy to roottools

--- core/python/helpers.py
+++ core/python/helpers.py
@@ -103,3 +103,67 @@ def clone(root_object, new_name = None):
 def add_to_sequence( func, sequence ):
     sequence.append( func )
     return func
+
+def renew_proxy( filename = None, rfc = False, request_time = 192, min_time = 0):
+    import os, subprocess
+
+    proxy = None
+    timeleft = 0
+
+    # Make voms-proxy-info look for a specific proxy
+    if filename is not None:
+        os.environ["X509_USER_PROXY"] = filename
+
+    # Check proxy path
+    try:
+        proxy     = read_from_subprocess( 'voms-proxy-info --path'.split() )[0]
+    except IndexError:
+        pass
+
+    try:
+        tl = read_from_subprocess( 'voms-proxy-info --timeleft'.split() )
+        timeleft = int(float( tl[0] ))
+    except IndexError:
+        pass
+    except ValueError:
+        print tl
+        pass
+
+    # Return existing proxy from $X509_USER_PROXY, the default location or filename
+    if proxy is not None and os.path.exists( proxy ):
+        if filename is None or os.path.abspath( filename ) == proxy:
+            logger.info( "Found proxy %s with lifetime %i hours", proxy, timeleft/3600)
+            if timeleft > 0 and timeleft >= min_time*3600 :
+                os.environ["X509_USER_PROXY"] = proxy
+                return proxy
+            else:
+                logger.info( "Lifetime %i not sufficient (require %i, will request %i hours).", timeleft/3600, min_time, request_time )
+
+    
+    arg_list = ['voms-proxy-init', '-voms', 'cms']
+
+    if filename is not None:
+        arg_list += [ '-out', filename ]
+
+    arg_list += ['--valid', "%i:0"%request_time ]
+
+    if rfc:
+        arg_list += ['-rfc']
+
+    # make proxy
+    p = subprocess.call( arg_list )
+
+    # read path
+    new_proxy = None
+    try:
+        new_proxy     = read_from_subprocess( 'voms-proxy-info --path'.split() )[0]
+    except IndexError:
+        pass
+
+    if new_proxy is not None and os.path.exists( new_proxy ):
+        os.environ["X509_USER_PROXY"] = new_proxy
+        logger.info( "Successfully created new proxy %s", new_proxy )
+        return new_proxy
+    else:
+        raise RuntimeError( "Failed to make proxy %s" % new_proxy )
+
