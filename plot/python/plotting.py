f9954c98e4f8d8e81eece9e0bc7429dc9df384c7
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-01-11 11:08:30 +0100

    adding canvas and histo modifications for draw2D

--- plot/python/plotting.py
+++ plot/python/plotting.py
@@ -235,7 +235,7 @@ def draw(plot, \
         legend: "auto" (default) or [x_low, y_low, x_high, y_high] or None. ([<legend_coordinates>], n) divides the legend into n columns. 
         drawObjects = [] Additional ROOT objects that are called by .Draw() 
         widths = {} (default) to update the widths. Values are {'y_width':500, 'x_width':500, 'y_ratio_width':200}
-        canvasModifications = [] could be used to pass on lambdas to modify the canvas
+        canvasModifications = [] could be used to pass on lambdas to modify the canvas. histModifications similar for histos.
         copyIndexPHP: whether or not to copy index.php to the plot directory
     '''
     # FIXME -> Introduces CMSSW dependence
@@ -567,8 +567,9 @@ def draw2D(plot, \
         logX = False, logY = False, logZ = True, 
         drawObjects = [],
         widths = {},
+        canvasModifications = [],
+        histModifications = [],
         copyIndexPHP = False,
-        optimizeLogZ = False,
         ):
     ''' plot: a Plot2D instance
         zRange: None ( = ROOT default) or [low, high] 
@@ -576,6 +577,7 @@ def draw2D(plot, \
         logX: True/False (default), logY: True/False(default), logZ: True/False(default)
         drawObjects = [] Additional ROOT objects that are called by .Draw() 
         widths = {} (default) to update the widths. Values are {'y_width':500, 'x_width':500, 'y_ratio_width':200}
+        canvasModifications = [] could be used to pass on lambdas to modify the canvas. histModifications similar for histos.
         copyIndexPHP: whether or not to copy index.php to the plot directory
     '''
 
@@ -607,6 +609,8 @@ def draw2D(plot, \
     c1.SetTopMargin(0.07)
     c1.SetRightMargin(0.05)
 
+    for modification in canvasModifications: modification(c1)
+
     drawOption = plot.drawOption if hasattr(plot, "drawOption") else "COLZ"
 
     c1.SetLogx(logX)
@@ -631,6 +635,8 @@ def draw2D(plot, \
     ROOT.niceColorPalette(255)
     #ROOT.gStyle.SetPalette(1)
     ROOT.gPad.SetRightMargin(0.15)
+
+    for modification in histModifications: modification(histo)
 
     histo.Draw(drawOption)
 
